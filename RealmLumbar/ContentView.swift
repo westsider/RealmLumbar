039e7e77211f1a0b7bd7d2a41d7592b27a097006
westsider <whansen1@mac.com>
2024-01-10 11:46:08 -0600

    persisting textfeilds

--- RealmLumbar/ContentView.swift
+++ RealmLumbar/ContentView.swift
@@ -13,23 +13,25 @@ struct ContentView: View {
     @ObservedResults(ShoppingList.self) var shoppingLists
     @State private var isPresented: Bool = false
     
-    // textfeild vars
-    @State private var title: String = ""
-    @State private var address: String = ""
     
     var body: some View {
         NavigationView {
+            
             VStack {
                 if shoppingLists.isEmpty {
                     Text("Shopping List is Empty")
                 }
+                Button("Delete Realm") {
+                    deleteRealm()
+                }
                 List {
                     ForEach(shoppingLists, id: \.id) { shoppingList in
-                        VStack(alignment: .leading) {
-                            Text(shoppingList.title)
-                            Text(shoppingList.address)
-                                .opacity(0.4)
-                        }
+//                        VStack(alignment: .leading) {
+//                            Text(shoppingList.title)
+//                            Text(shoppingList.address)
+//                                .opacity(0.4)
+//                        }
+                        LumbarRow(id: shoppingList.id)
                         
                     }
                 }.navigationTitle("Grocery App")
@@ -47,8 +49,43 @@ struct ContentView: View {
                     }
                     
                 }
+            }.onAppear() {
+                if shoppingLists.isEmpty {
+                        generateDefaultObjecs()
+                }
             }
+            
         }
+    }
+    
+    func generateDefaultObjecs()  {
+        let retrievedObject = ShoppingList()
+        retrievedObject.title = "Gen 1"
+        retrievedObject.address = "123 lax blvd"
+        $shoppingLists.append(retrievedObject)
+        
+        let retrievedObject2 = ShoppingList()
+        retrievedObject2.title = "Gen 2"
+        retrievedObject2.address = "911 n mill st"
+        $shoppingLists.append(retrievedObject2)
+    }
+    
+    private func showItems() {
+//        ForEach(shoppingLists, id: \.id) { shoppingList in
+//            Text(shoppingList.title)
+//            Text(shoppingList.id)
+//        }
+//        ForEach(shoppingLists) { list in
+//            print(list.id)
+//        }
+    }
+    
+    private func deleteRealm() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+        generateDefaultObjecs()
     }
 }
 
